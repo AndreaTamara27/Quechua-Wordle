554e884de100f119e94a60817ab1ca9b54adfc31
AndreaTamara27 <118624048+AndreaTamara27@users.noreply.github.com>
2022-11-19 16:16:08 -0400

    Palabras en Quechua
    Palabras de 5

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -13,7 +13,7 @@ export const CONFIG = {
   shuffle: true, // whether to shuffle the words in the wordlist each time you load the app (note: you will lose the 'word of the day' functionality if this is true)
   normalization: 'NFC', // whether to apply Unicode normalization to words and orthography - options: 'NFC', 'NFD', 'NKFC', 'NKFD', false
   startDate: 'January 1, 2022 00:00:00', // what date and time to start your game from
-  defaultLang: 'qh', // the default interface language
-  availableLangs: ['ay', 'es','en', 'sw', 'zh', 'qh'], // the options available to the user for translation languages
+  defaultLang: 'qu', // the default interface language
+  availableLangs: ['ay', 'es','en', 'sw', 'zh', 'qu'], // the options available to the user for translation languages
   escapeSpecialCharacters: true, // whether to escape all characters in the orthography.
 }
